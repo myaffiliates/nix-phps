f4ff71901bc09927b57362d82afa08386dcbc2c8
Edward Palmer <edward@myaffiliates.com>
2024-09-17 22:37:07 +1000

    trying to fix intl

--- pkgs/package-overrides.nix
+++ pkgs/package-overrides.nix
@@ -297,7 +297,7 @@ in
         if lib.versionOlder prev.php.version "8.1.0" then
           (builtins.filter (pkg: pkg != pkgs.icu73) attrs.buildInputs) ++ [ pkgs.icu64 ]
         else if lib.versionOlder prev.php.version "8.2.0" then
-           (builtins.filter (pkg: pkg != pkgs.icu73) attrs.buildInputs) ++ [ pkgs.icu74 ]
+           (builtins.filter (pkg: pkg != pkgs.icu74) attrs.buildInputs) ++ [ pkgs.icu64 ]
         else
           attrs.buildInputs;
       doCheck = if lib.versionOlder prev.php.version "7.2" then false else attrs.doCheck or true;
